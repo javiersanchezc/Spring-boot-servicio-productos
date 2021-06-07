b838d2d90703824f280a21891043907c1d713990
jscoder <javier.ant.sanchezc@gmail.com>
2021-06-07 11:15:49 -0500

    Initial commit

---
+++ src/main/java/com/jscoder/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,43 @@
+package com.jscoder.springboot.app.productos.models.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+    private  Double precio;
+    private Date  fecha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
